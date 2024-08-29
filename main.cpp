a9c5e9c5f74847520ae57f934a6ec31f05d27df1
Andre <andreabouhabib@gmail.com>
2024-08-28 20:56:27 -0700

    First Commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Andre Abou-Habib
+ * Date: August 28th, 2024
+ * Purpose: "Hello World" program
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    cout << "Hello world!";
+    
+    return 0;
+}
+
